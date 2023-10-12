0c913c017b8558286646c38c3ab40f1f0226383c
Agustin Lopez <agustinlpzb@gmail.com>
2023-10-12 17:37:09 +0200

    styling the navigation bar

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Index.css";
 import DaiLogo from "../../assets/Dailogo-nobg.png";
+import HomeImg from "../../assets/home.svg";
+import AboutImg from "../../assets/about.svg";
+import ProductImg from "../../assets/products.svg";
+import ContactImg from "../../assets/contact.svg";
 
 function Home() {
   return (
@@ -10,15 +14,19 @@ function Home() {
           <img src={DaiLogo} alt="Dainippon" />
           <ul>
             <li>
+              <a href="./Inicio"><img src={HomeImg} alt="Inicio-img" /></a>
               <a href="./Inicio">Inicio</a>{" "}
             </li>
             <li>
+              <a href="./Empresa"><img src={AboutImg} alt="Empresa-img" /></a>
               <a href="./Empresa">Nuestra empresa</a>{" "}
             </li>
             <li>
+              <a href="./Productos"><img src={ProductImg} alt="Productos-img" /></a>
               <a href="./Productos">Productos</a>{" "}
             </li>
             <li>
+              <a href="./Contacto"><img src={ContactImg} alt="Contacto-img" /></a>
               <a href="./Contacto">Contacto</a>
             </li>
           </ul>
